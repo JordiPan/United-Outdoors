96e4ed89c53d921cd9420bf4be65c721bb0c98a7
Xavier8L <shenglongchen99@gmail.com>
2024-05-27 16:53:36 +0200

    setting verandere

--- src/settings.py
+++ src/settings.py
@@ -5,11 +5,7 @@ class Settings():
     basedir = Path.cwd()
     processeddir = Path("../data/raw/")
     logdir = basedir / "log"
-<<<<<<< HEAD
-    servername = 'LAPTOP-4SN45QMD\SQLEXPRESS'
-=======
     servername = 'LAPTOP-C1FMPSTV\SQLEXPRESS01'
->>>>>>> dashbord
     database = 'UnitedOutdoors'
 
 settings = Settings()
